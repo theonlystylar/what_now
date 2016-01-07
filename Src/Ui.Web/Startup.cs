f16e7e699ae548cd06037037ce5e92cd2bdd634d
theonlystylar <andy.jeffrey@intel.com>
2016-01-06 17:04:57 -0800

    Ui.Web empty asp.net web project added and MVC enabled.

---
+++ Src/Ui.Web/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Ui.Web
+{
+	public class Startup
+	{
+		public static IConfigurationRoot Configuration;
+
+		public Startup(IApplicationEnvironment appEnv)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(appEnv.ApplicationBasePath)
+				.AddJsonFile("config.json")
+				.AddEnvironmentVariables();
+
+			Configuration = builder.Build();
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseStaticFiles();
+			app.UseMvc(config =>
+			{
+				config.MapRoute(
+					name: "Default",
+					template: "{controller}/{action}/{id?}",
+					defaults: new { controller = "Client", action = "Index" }
+					);
+			});
+		}
+
+		// Entry point for the application.
+		public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+	}
+}
