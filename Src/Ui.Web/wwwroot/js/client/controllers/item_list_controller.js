cb309d589243989fb3b7522a9c4eaf2c9b976d81
theonlystylar <andy.jeffrey@intel.com>
2016-01-09 00:04:07 -0800

    Added Toastr to notify user when a item log has been saved

--- Src/Ui.Web/wwwroot/js/client/controllers/item_list_controller.js
+++ Src/Ui.Web/wwwroot/js/client/controllers/item_list_controller.js
@@ -1,15 +1,22 @@
 ﻿var clientAppControllers = angular.module("clientAppControllers", []);
 
 clientAppControllers.controller("ItemListController", [
-	"$scope", "ItemLogService", "ItemQueryService", function($scope, itemLogService, itemQueryService) {
-
+	"$scope", "ItemLogService", "ItemQueryService", function ($scope, itemLogService, itemQueryService) {
 		itemQueryService.getRoot(function (data) {
 			$scope.items = data;
 		})
 
-		$scope.onClick = function(item) {
+		$scope.onClick = function (item) {
 			itemLogService.save({
-				itemId: item.id
+				itemId: item.id,
+			},
+			// success
+			function () {
+				toastr["success"]('Logged ' + item.name);
+			},
+			// error
+			function (error) {
+				toastr["error"]('Error!');
 			});
 		};
 	}
